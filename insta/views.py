65168268b1d126b0f3e49c044032b740ba45b576
Lulu <lulumutuli24@gmail.com>
2018-05-10 13:41:39 +0300

    setting up email configuration

--- insta/views.py
+++ insta/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-
+from .forms import InstaLetterForm
 #................
 # Create your views here.
 def welcome(request):
-    return render(request, 'gram/newsfeed.html')
+
+    if request.method == 'POST':
+        form = InstaLetterForm(request.POST)
+        if form.is_valid():
+            name= form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+            recipient = InstaLetterRecipients(name = name, email=email)
+            recipient.save()
+            HttpResponseRedirect('welcome')
+    else:
+        form = InstaLetterForm()
+    return render(request, 'gram/newsfeed.html',{"letterForm" : form})
--- instagram/settings.py
+++ instagram/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'insta.apps.InstaConfig',
+    'bootstrap3',
 
 ]
 
@@ -80,8 +81,8 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instagram',
         'USER' : 'lulumutuli',
         'PASSWORD' : 'lulu',
     }
